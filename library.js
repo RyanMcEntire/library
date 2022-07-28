83691578654be75ffff143347da22d20d3d27134
Ryan McEntire <ryanmcentire89@gmail.com>
2022-07-28 17:21:24 -0600

    add broken feature that breaks everything.

--- library.js
+++ library.js
@@ -23,15 +23,17 @@ const pages = document.getElementById("pages");
 const read = document.getElementById("readBook");
 const addButton = document.getElementById("addToLibrary");
 const newBook = document.getElementById("newBook");
+const deleteButton = document.getElementById("deleteButton");
 
 addButton.addEventListener("click", addBookToLibrary);
 newBook.addEventListener("click", centerForm);
+deleteButton.addEventListener("click", deleteBook);
 
 function centerForm() {
   document.getElementById("bookForm").className = "centerForm";
 }
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -39,10 +41,17 @@ function Book(title, author, pages, read) {
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${read}.`;
   };
+  this.index = index;
 }
 
 function addBookToLibrary() {
-  let newBook = new Book(title.value, author.value, pages.value, read.checked);
+  let newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked,
+    index
+  );
   myLibrary.push(newBook);
   addCardToShelf();
   document.getElementById("bookForm").className = "hideForm";
@@ -60,23 +69,19 @@ function randomHsl() {
 function createCard(book) {
   const bookCard = document.createElement("div");
   bookCard.classList.add("bookCard");
-  bookCard.style.backgroundColor = randomHsl()
+  bookCard.style.backgroundColor = randomHsl();
   bookShelf.appendChild(bookCard);
   const title = document.createElement("div");
   title.classList.add("title");
   title.textContent = book.title;
-  title.textContent = book.title;
-  console.log(Book.title);
   bookCard.appendChild(title);
   const author = document.createElement("div");
   author.classList.add("author");
   author.textContent = book.author;
-  console.log(Book.title);
   bookCard.appendChild(author);
   const pages = document.createElement("div");
   pages.classList.add("pages");
   pages.textContent = book.pages + " " + "pages";
-  console.log(Book.title);
   bookCard.appendChild(pages);
   const read = document.createElement("div");
   read.classList.add("read");
@@ -88,6 +93,10 @@ function createCard(book) {
     bookCard.appendChild(read);
   } else read.textContent = book.read;
   bookCard.appendChild(read);
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "deleteButton")
+  deleteButton.textContent = "delete";
+  bookCard.appendChild(deleteButton);
 }
 
 function addCardToShelf() {
@@ -97,5 +106,15 @@ function addCardToShelf() {
 
 myLibrary.push(everydayThings, asILayDying);
 
+function deleteBook() {
+  myLibrary.splice(myLibrary[index]);
+}
+
+const index = myLibrary.findIndex((object) => {
+  object.title == `${this.title}`;
+});
+
 // addBookToLibrary();
 addCardToShelf();
+
+console.log(myLibrary);
