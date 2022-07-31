747bf5a2fa2814ef972d2438ca3d100be231c2da
Ryan McEntire <ryanmcentire89@gmail.com>
2022-07-30 21:38:06 -0600

    add checkbox to card

--- library.js
+++ library.js
@@ -13,15 +13,15 @@ newBook.addEventListener("click", centerForm);
 
 bookShelf.addEventListener("click", (e) => {
   //To delete from array and DOM
+
   if (e.target.classList.contains("deleteButton")) {
     let bookToDelete = e.target.value;
     const cardIDToDelete = e.target.id;
-    console.log(e.target.value);
     let indexToDelete = myLibrary.findIndex((Book) => {
       return Book.title === bookToDelete;
     });
     const cardForDeletion = document.getElementById(`${bookToDelete}1`);
-    console.log(`${cardIDToDelete}1`);
+
     cardForDeletion.remove();
 
     myLibrary.splice([indexToDelete], 1);
@@ -46,10 +46,8 @@ function addBookToLibrary() {
   let newBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(newBook);
   addCardToShelf();
-  console.log(myLibrary);
   document.getElementById("bookForm").className = "hideForm";
-  // clear fields on submit
-  title.value = "";
+  title.value = ""; // clear fields on submit
   author.value = "";
   pages.value = "";
   readBook.checked = false;
@@ -77,16 +75,16 @@ function createCard(book) {
   pages.classList.add("pages");
   pages.textContent = book.pages + " " + "pages";
   bookCard.appendChild(pages);
-  const read = document.createElement("div");
+  const readBox = document.createElement("div");
+  readBox.classList.add("readBox");
+  readBox.textContent = "read";
+  bookCard.appendChild(readBox);
+  const read = document.createElement("input");
+  read.setAttribute("type", "checkbox");
   read.classList.add("read");
-  if (book.read) {
-    read.textContent = "Read";
-    bookCard.appendChild(read);
-  } else if (!book.read) {
-    read.textContent = "Unread";
-    bookCard.appendChild(read);
-  } else read.textContent = book.read;
-  bookCard.appendChild(read);
+  read.setAttribute("value", book.read)
+  readBox.appendChild(read);
+  
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("id", "deleteButton");
   deleteButton.classList.add("deleteButton");
@@ -98,5 +96,3 @@ function createCard(book) {
 function addCardToShelf() {
   createCard(myLibrary[myLibrary.length - 1]);
 }
-
-console.log(myLibrary);
