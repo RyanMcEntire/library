15bcae784d96cd48a8bc5a0032c22462d34b9e93
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-04-02 23:35:54 -0600

    make attempt at CV

--- library.js
+++ library.js
@@ -1,20 +1,23 @@
 let myLibrary = [];
-const bookShelf = document.querySelector("#actualShelf");
-const bookForm = document.querySelector("#bookForm");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const read = document.getElementById("readBook");
-const addButton = document.getElementById("addToLibrary");
-const newBook = document.getElementById("newBook");
+const bookShelf = document.querySelector('#actualShelf');
+const bookForm = document.querySelector('#bookForm');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('readBook');
+const addButton = document.getElementById('addToLibrary');
+const newBook = document.getElementById('newBook');
+const titleError = document.getElementById('title-error');
+const authorError = document.getElementById('author-error');
+const pagesError = document.getElementById('pages-error');
 
-addButton.addEventListener("click", addBookToLibrary);
-newBook.addEventListener("click", centerForm);
+addButton.addEventListener('click', addBookToLibrary);
+newBook.addEventListener('click', centerForm);
 
-bookShelf.addEventListener("click", (e) => {
+bookShelf.addEventListener('click', (e) => {
   //To delete from array and DOM
 
-  if (e.target.classList.contains("deleteButton")) {
+  if (e.target.classList.contains('deleteButton')) {
     let bookToDelete = e.target.value;
     const cardIDToDelete = e.target.id;
     let indexToDelete = myLibrary.findIndex((Book) => {
@@ -28,8 +31,8 @@ bookShelf.addEventListener("click", (e) => {
   }
 });
 
-bookShelf.addEventListener("change", (e) => {
-  if (e.target.classList.contains("read")) {
+bookShelf.addEventListener('change', (e) => {
+  if (e.target.classList.contains('read')) {
     let bookToChange = e.target.value;
     let indexToChange = myLibrary.findIndex((Book) => {
       return Book.title === bookToChange;
@@ -44,7 +47,7 @@ bookShelf.addEventListener("change", (e) => {
 });
 
 function centerForm() {
-  document.getElementById("bookForm").className = "centerForm";
+  document.getElementById('bookForm').className = 'centerForm';
 }
 
 function Book(title, author, pages, read) {
@@ -61,55 +64,55 @@ function addBookToLibrary() {
   let newBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(newBook);
   addCardToShelf();
-  document.getElementById("bookForm").className = "hideForm";
-  title.value = ""; // clear fields on submit
-  author.value = "";
-  pages.value = "";
+  document.getElementById('bookForm').className = 'hideForm';
+  title.value = ''; // clear fields on submit
+  author.value = '';
+  pages.value = '';
   readBook.checked = false;
 }
 
 function randomHsl() {
-  return "hsla(" + Math.random() * 360 + ", 70%, 90%, 1)";
+  return 'hsla(' + Math.random() * 360 + ', 70%, 90%, 1)';
 }
 
 function createCard(book) {
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("bookCard");
-  bookCard.setAttribute("id", `${book.title}1`);
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('bookCard');
+  bookCard.setAttribute('id', `${book.title}1`);
   bookCard.style.backgroundColor = randomHsl();
   bookShelf.appendChild(bookCard);
-  const title = document.createElement("div");
-  title.classList.add("title");
+  const title = document.createElement('div');
+  title.classList.add('title');
   title.textContent = book.title;
   bookCard.appendChild(title);
-  const author = document.createElement("div");
-  author.classList.add("author");
+  const author = document.createElement('div');
+  author.classList.add('author');
   author.textContent = book.author;
   bookCard.appendChild(author);
-  const pages = document.createElement("div");
-  pages.classList.add("pages");
-  if (book.pages !== "") pages.textContent = book.pages + " " + "pages";
+  const pages = document.createElement('div');
+  pages.classList.add('pages');
+  if (book.pages !== '') pages.textContent = book.pages + ' ' + 'pages';
   bookCard.appendChild(pages);
-  const readBox = document.createElement("div");
-  readBox.classList.add("readBox");
-  readBox.textContent = "read";
+  const readBox = document.createElement('div');
+  readBox.classList.add('readBox');
+  readBox.textContent = 'read';
   bookCard.appendChild(readBox);
-  const read = document.createElement("input");
-  read.setAttribute("type", "checkbox");
-  read.setAttribute("value", `${book.title}`);
-  read.classList.add("read");
+  const read = document.createElement('input');
+  read.setAttribute('type', 'checkbox');
+  read.setAttribute('value', `${book.title}`);
+  read.classList.add('read');
   if (book.read === true) {
-    read.setAttribute("checked", "checked");
+    read.setAttribute('checked', 'checked');
   } else if (book.read === false) {
-    read.removeAttribute("checked");
+    read.removeAttribute('checked');
   }
   readBox.appendChild(read);
 
-  const deleteButton = document.createElement("button");
-  deleteButton.setAttribute("id", "deleteButton");
-  deleteButton.classList.add("deleteButton");
-  deleteButton.setAttribute("value", `${book.title}`);
-  deleteButton.textContent = "delete";
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('id', 'deleteButton');
+  deleteButton.classList.add('deleteButton');
+  deleteButton.setAttribute('value', `${book.title}`);
+  deleteButton.textContent = 'delete';
   bookCard.appendChild(deleteButton);
 }
 
